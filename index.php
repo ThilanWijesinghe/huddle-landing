ec8525e9cc17e1aacbf980e104a99d6f428faaf4
Thilan Wijesinghe <thilan.ruwanga665@gmail.com>
2023-02-20 16:52:21 +0530

    first commit

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="css/bootstrap.css">
+    <title>Huddle-Landing</title>
+    <link rel="icon" href="image/favicon-32x32.png">
+   
+</head>
+
+
+<body class="bg">
+
+
+    
+</body>
+</html>
