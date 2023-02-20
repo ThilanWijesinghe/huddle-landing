0c869775241431c6afb6d26d74ac4f89c69e140f
Thilan Wijesinghe <thilan.ruwanga665@gmail.com>
2023-02-20 21:20:57 +0530

    done

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -9,13 +10,60 @@
     <link rel="stylesheet" href="css/bootstrap.css">
     <title>Huddle-Landing</title>
     <link rel="icon" href="image/favicon-32x32.png">
-   
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.2/font/bootstrap-icons.css">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+
+
 </head>
 
 
 <body class="bg">
 
+    <div class="container-fluid">
+        <img class=" mt-5 m-lg-5" src="image/logo.svg" alt="">
 
-    
+        <div class="col-12 col-lg-6  d-block d-md-flex ">
+            <div class="row col-12">
+                <img class="col-12 col-lg-11 m-lg-5 " src="image/illustration-mockups.svg" alt="">
+            </div>
+
+
+            <div class=" row col-12  ">
+
+                <span class=" t1 mt-5 mb-1 d-none d-md-flex offset-1 ">Build The Community <br> Your Fans Will Love</span>
+                <span class=" t2  d-md-none text-center mb-3 mt-5 ">Build The Community <br> Your Fans Will Love</span>
+
+                <p class="p1 mb-5 d-none d-md-flex offset-1">Huddle re-imagines the way we build communities. You have <br> a voice, but so does your audience. Create connections with <br> your users as you engage in genuine discussion.</p>
+                <p class="p1 d-md-none text-center">Huddle re-imagines the way we build communities. You have  a voice, but so does your audience. Create connections with  your users as you engage in genuine discussion.</p>
+
+                <button class="btn1 col-9  mb-lg-5 offset-m-2 offset-1 col-lg-3">Register</button>
+
+                <div class="icon  ">
+
+                    <ul>
+                        <a href="https://www.facebook.com/thilan.ruwanga"><i class="fa fa-facebook"></i></a>&nbsp;&nbsp;
+                        <a href="https://twitter.com/Thilan_twi"><i class="bi bi-twitter"></i></a>&nbsp;&nbsp;
+                        <a href="https://discord.com/channels/@me"><i class="bi bi-instagram"></i></a>&nbsp;&nbsp;
+
+                    </ul>
+
+
+                </div>
+
+                
+            </div>
+
+        </div>
+
+
+
+
+
+    </div>
+    </div>
+
+
+
 </body>
+
 </html>
